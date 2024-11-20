c78783d044baf6932a10ac564b98af6d7e713339
Douglas Borges <douglasborges19@gmail.com>
2024-11-20 15:57:11 -0300

    Testando as Queries

---
+++ PaymentContext.Tests/Queries/StudentQueriesTests.cs
@@ -0,0 +1,42 @@
+﻿using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Queries;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests.Queries
+{
+    [TestClass]
+    public class StudentQueriesTests
+    {
+        private IList<Student> _students = new List<Student>();
+
+        public StudentQueriesTests()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                _students.Add(new Student(
+                    new Name("Aluno", i.ToString()),
+                    new Document("1111111111" + i.ToString(), Domain.Enums.EDocumentType.CPF),
+                    new Email(i.ToString() + "@balta.io")
+                ));
+            }
+        }
+
+        [TestMethod]
+        public void ShouldReturnNullWhenDocumentNotExists()
+        {
+            var exp = StudentQueries.GetStudentInfo("1234567890");
+            var student = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.IsNull(student);
+        }
+
+        [TestMethod]
+        public void ShouldReturnStudentWhenDocumentNotExists()
+        {
+            var exp = StudentQueries.GetStudentInfo("11111111111");
+            var student = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.IsNotNull(student);
+        }
+    }
+}
